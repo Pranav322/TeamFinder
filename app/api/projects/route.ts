5c5d93c5d1fa2d515bd59dc073810b88f5698574
Pranav <105759613+Pranav322@users.noreply.github.com>
2025-01-16 17:54:48 +0530

    Update route.ts

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,16 +1,13 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
-    if (req.method === 'GET') {
-        try {
-            const projects = await prisma.project.findMany();
-            return NextResponse.json(projects);
-        } catch (error) {
-            return NextResponse.json({ error: error.message });
-        }
-    } else {
-        return NextResponse.json({ error: 'Invalid request method' });
+    try {
+        const projects = await prisma.project.findMany();
+        return NextResponse.json(projects);
+    } catch (error) {
+        // Narrow down the error type
+        const message = error instanceof Error ? error.message : 'Unknown error occurred';
+        return NextResponse.json({ error: message }, { status: 500 });
     }
 }
