281369560f419d065aaded013592bc63352223e2
Pranav <105759613+Pranav322@users.noreply.github.com>
2025-01-16 17:49:45 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
